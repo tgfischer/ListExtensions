1da07933818248ce68106c9908a08bf5ebe5d394
Tom Fischer <tfische5@uwo.ca>
2014-10-18 21:11:29 -0400

    Added extensions for
    1) Finding all permutations of a list
    2) Swapping two items in a list

---
+++ ListExtensions/ListExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExtensions
+{
+    public static class ListExtensions
+    {
+        public static List<List<T>> GetPermutations<T>(this List<T> list)
+        {
+            List<List<T>> permutations = new List<List<T>>();
+            Recurse<T>(permutations, list, 0, list.Count - 1);
+            return permutations;
+        }
+
+        private static void Recurse<T>(List<List<T>> permutations, List<T> list, int j, int k)
+        {
+            if (j == k)
+            {
+                permutations.Add(list.ToList());
+            }
+            else
+            {
+                for (int i = j; i <= k; i++)
+                {
+                    Swap(list, j, i);
+                    Recurse(permutations, list, j + 1, k);
+                    Swap(list, j, i);
+                }
+            }
+        }
+
+        public static void Swap<T>(this List<T> list, int A, int B)
+        {
+            T item = list[A];
+            list[A] = list[B];
+            list[B] = item;
+        }
+    }
+}
---
+++ ListExtensions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                list.Add(i);
+            }
+
+            List<List<int>> permutations = list.GetPermutations();
+
+            foreach (List<int> permutation in permutations)
+            {
+                for (int i = 0; i < permutation.Count; i++)
+                {
+                    Console.Write(permutation[i]);
+
+                    if (i < permutation.Count - 1) Console.Write(", ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n" + permutations.Count);
+            Console.ReadLine();
+        }
+    }
+}
